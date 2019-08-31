d38fe55a17028c4bca5e12177664d0609294a0d0
Nick Wang <nickyiliwang@gmail.com>
2019-08-31 11:27:18 -0400

    auth

--- src/component/App.js
+++ src/component/App.js
@@ -3,17 +3,32 @@ import Header from "./Header";
 import AddTodo from "../containers/AddTodo";
 import { VisibilityToggle } from "./VisibilityToggle";
 import RenderList from "../containers/RenderList";
-
-// err
+import { Auth } from "../containers/Auth";
 
 class App extends React.Component {
   render() {
     return (
-      <div className="ui container center aligned">
-        <Header />
-        <AddTodo />
-        <VisibilityToggle />
-        <RenderList />
+      <div className="ui equal width center aligned padded grid">
+        <div className="row">
+          <div className="column">
+            <Header />
+          </div>
+          <div className="column">
+            <Auth />
+          </div>
+        </div>
+        <div className="row">
+          <div className="column">
+            <AddTodo />
+          </div>
+        </div>
+        <div className="row">
+          <div className="column">
+            {" "}
+            <VisibilityToggle />
+            <RenderList />
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/containers/Auth.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Auth = () => {
+  return (
+    <div>
+      <button>Google</button>
+      <button>Register</button>
+    </div>
+  );
+};
